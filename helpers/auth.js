588d8373548ee36e081e267aef91f0f252041c10
Lucas Soares <lucasoaresleite@gmail.com>
2021-02-18 08:46:00 -0300

    set ath

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,3 +2,29 @@ const bcrypt = require('bcryptjs');
 const localStrategy = require('passport-local').Strategy;
 
 //Models
+const Usuario = require('../models/Usuario.model');
+module.exports = (passport)=>{
+    passport.use(
+        new localStrategy({
+            {apelidoField: 'apelido', passwordField: 'senha'},(apelido,senha,done)=>{
+                Usuario.findOne({
+                    where:{ apelido: apelido}
+                }).then(
+                    (apelido)=>{
+                        if(!apelido){
+                            return done(null,false,{message: "Login e/ou senha inválidos."})
+                        }
+                        bcrypt.compare(senha,apelido.senha, (erro,equalsPassword)=>{
+                            if(equalsPassword){
+                                return done(null,apelido)
+                            }else{
+                                return done(null,false, {message: "Login e/ou senha inválidos"})
+                            }
+                        })
+                    }
+                )
+            }
+        })
+
+    )
+}
