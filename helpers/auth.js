c05fe316218a1b817609c8e852df6559188ce20b
Lucas Soares <lucasoaresleite@gmail.com>
2021-02-18 13:49:58 -0300

    auth

--- helpers/auth.js
+++ helpers/auth.js
@@ -3,28 +3,28 @@ const localStrategy = require('passport-local').Strategy;
 
 //Models
 const Usuario = require('../models/Usuario.model');
-module.exports = (passport)=>{
+module.exports = (passport) => {
     passport.use(
-        new localStrategy({
-            {apelidoField: 'apelido', passwordField: 'senha'},(apelido,senha,done)=>{
+        new localStrategy(
+            { apelidoField: 'apelido', passwordField: 'senha' }, (apelido, senha, done) => {
                 Usuario.findOne({
-                    where:{ apelido: apelido}
-                }).then(
-                    (apelido)=>{
-                        if(!apelido){
-                            return done(null,false,{message: "Login e/ou senha inválidos."})
-                        }
-                        bcrypt.compare(senha,apelido.senha, (erro,equalsPassword)=>{
-                            if(equalsPassword){
-                                return done(null,apelido)
-                            }else{
-                                return done(null,false, {message: "Login e/ou senha inválidos"})
+                    where: { apelido: apelido }
+                })
+                    .then(
+                        (apelido) => {
+                            if (!apelido) {
+                                return done(null, false, { message: "Login e/ou senha inválidos." })
                             }
-                        })
-                    }
-                )
+                            bcrypt.compare(senha, apelido.senha, (erro, equalsPassword) => {
+                                if (equalsPassword) {
+                                    return done(null, apelido)
+                                } else {
+                                    return done(null, false, { message: "Login e/ou senha inválidos" })
+                                }
+                            })
+                        }
+                    )
             }
-        })
-
-    )
+        ))
 }
+
